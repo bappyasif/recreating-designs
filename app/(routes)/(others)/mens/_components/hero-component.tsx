3b83cad0b6f10b901e1f398c64fda0a98fb71857
bappyasif <bappyasif@hotmail.com>
2024-04-23 21:31:35 +0600

    added components
    * added styles to hero components
    * added components to sliders

--- app/(routes)/(others)/mens/_components/hero-component.tsx
+++ app/(routes)/(others)/mens/_components/hero-component.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 export const HeroComponent = () => {
   return (
-    <div>
+    <div className='px-4 flex flex-col gap-y-20'>
         <Header />
-        <div className='flex justify-between px-4'>
+        <div className='flex justify-between'>
             <LeftSide />
             <RightSide />
         </div>
@@ -16,7 +16,7 @@ export const HeroComponent = () => {
 
 const RightSide = () => {
     return (
-        <Image src={"https://howies.co.uk/cdn/shop/files/HOME_2024_DENIM_5de2b43c-c0b6-47dc-85b1-dc070007faa1.jpg?v=1708964775"} width={1100} height={800} alt='' className='w-1/2 px-20' />
+        <Image src={"https://howies.co.uk/cdn/shop/files/HOME_2024_DENIM_5de2b43c-c0b6-47dc-85b1-dc070007faa1.jpg?v=1708964775"} width={1100} height={800} alt='' className='w-1/2 px-20 h-[40rem]' />
     )
 }
 
@@ -24,9 +24,9 @@ const LeftSide = () => {
     const insetMarkup = () => {
         return (
             <div className='w-1/2'>
-                <Link href={"#"}>Our fabric stories</Link>
+                <Link href={"#"} className='underline text-2xl'>Our fabric stories</Link>
                 <div>
-                    <Link href={"#"}>Read more</Link>
+                    <Link href={"#"} className='text-xl text-slate-400'>Read more</Link>
                     <span>&gt;</span>
                 </div>
             </div>
@@ -34,8 +34,8 @@ const LeftSide = () => {
     }
 
     return (
-        <div>
-            <p>All the outdoor staples you need; discover our latest tops, trousers, jackets and more right here. All made using lower impact natural and sustainable fibres for comfort and functionality.</p>
+        <div className='flex flex-col justify-between'>
+            <p className='text-5xl text-slate-400'>All the outdoor staples you need; discover our latest tops, trousers, jackets and more right here. All made using lower impact natural and sustainable fibres for comfort and functionality.</p>
             {insetMarkup()}
         </div>
     )
@@ -45,6 +45,6 @@ const Header = () => {
     const getYear = () => new Date().getFullYear().toString().split("").map((v, i) => i > 1 && v).filter(v=> v).join("")
 
     return (
-        <h2>SS/{getYear()}</h2>
+        <h2 className='text-4xl'>Men's SS/{getYear()}</h2>
     )
 }
--- app/(routes)/(others)/mens/_components/product-slider.tsx
+++ app/(routes)/(others)/mens/_components/product-slider.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+"use client"
+
+import React, { useRef, useState } from 'react'
 
 export const ProductSlider = ({ headerText }: { headerText: string }) => {
 
     return (
-        <div className='flex flex-col gap-y-6'>
-            <div className='flex justify-between px-4 gap-x-4'>
-                <h2>{headerText}</h2>
-                <button>View all</button>
+        <div className='flex flex-col gap-y-6 ml-20'>
+            <div className='flex justify-between gap-x-4 font-bold mr-20'>
+                <h2 className='text-4xl'>{headerText}</h2>
+                <button className='text-xl'>View all</button>
             </div>
             <SliderScroller />
         </div>
@@ -14,14 +16,85 @@ export const ProductSlider = ({ headerText }: { headerText: string }) => {
 }
 
 const SliderScroller = () => {
-    const allProds = () => [1, 2, 3, 4, 5, 6].map(v => <div key={v} className='bg-slate-400 min-h-[27rem] min-w-96 rounded-sm'>{v}</div>)
+    const ref = useRef<HTMLDivElement | null>(null)
+
+    const [count, setCount] = useState(0)
+
+    const MAXSCROLLED = 6 * 384
+
+    const [totalScrolled, setTotalScrolled] = useState(0)
+
+    const handleNext = () => {
+        // console.log(ref.current, count, "start next")
+        if (ref.current && count < 3) {
+            // console.log(ref.current, count, "inside next")
+            ref.current.scrollLeft += 384
+            setCount(prev => prev + 1)
+
+            setTotalScrolled(prev => prev + 384)
+        }
+    }
+
+    const handlePrev = () => {
+        // console.log(ref.current, count, "start prev")
+        if (ref.current && count > 0) {
+            // console.log(ref.current, count, "inside prev")
+            ref.current.scrollLeft -= 384
+            setCount(prev => prev - 1)
+
+            setTotalScrolled(prev => prev - 384)
+        }
+    }
+
+    const allProds = () => [1, 2, 3, 4, 5, 6, 7].map(v => <div key={v} className='bg-slate-400 min-h-[27rem] min-w-96 rounded-sm'>{v}</div>)
 
     return (
-        <div className='w-full flex flex-col gap-y-6'>
-            <div className='flex gap-4 flex-nowrap overflow-x-scroll w-full'>
+        <div
+            className='w-full flex flex-col gap-y-6 items-center justify-between relative'
+            // className='w-full h-[40rem] flex flex-col gap-y-6'
+            // className='space-y-6'
+        >
+            <div
+                ref={ref}
+                className='flex gap-4 flex-nowrap overflow-x-hidden w-full'
+                // className='flex items-center justify-start gap-4 flex-nowrap overflow-x-hidden w-full relative'
+            >
                 {allProds()}
+
+                {/* <SwipperButtons handleNext={handleNext} handlePrev={handlePrev} /> */}
             </div>
+            <SwipperButtons handleNext={handleNext} handlePrev={handlePrev} />
             <div className='w-full text-center'>slider!!</div>
+
+            <SliderScrollBar totalScrolled={totalScrolled} />
+        </div>
+    )
+}
+
+const SwipperButtons = ({ handleNext, handlePrev }: { handleNext: () => void, handlePrev: () => void }) => {
+    return (
+        <div className='w-full flex justify-between items-center h-full absolute'>
+            <button className='bg-slate-800 rounded-full w-16 h-16' onClick={handlePrev}>&lt;</button>
+            <button className='mr-20 bg-slate-800 rounded-full w-16 h-16' onClick={handleNext}>&gt;</button>
+        </div>
+    )
+}
+
+const SliderScrollBar = ({totalScrolled}: {totalScrolled: number}) => {
+    const ref = useRef<HTMLDivElement | null>(null)
+
+    console.log(totalScrolled, "totalScrolled!!", `ml-[${totalScrolled}px]`, totalScrolled/66.7, totalScrolled * 66.7/100)
+
+    return (
+        <div className='w-2/3 mx-auto bg-slate-400 h-2'>
+            <div 
+                className='h-2 w-60 bg-slate-800'
+                // className={`h-2 w-60 bg-slate-800 ml-[${totalScrolled}px]`}
+                style={{
+                    // marginLeft: `${totalScrolled}px`
+                    marginLeft: `${totalScrolled * 66.7/100}px`
+                }}
+            ></div>
         </div>
     )
 }
