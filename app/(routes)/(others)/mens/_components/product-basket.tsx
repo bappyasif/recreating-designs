4fcc21018b9b018f2560750f55589fc7a182b00a
bappyasif <bappyasif@hotmail.com>
2024-04-26 01:44:21 +0600

    adjusted ui
    * adjusted scroller ui and add to basket component

--- app/(routes)/(others)/mens/_components/product-basket.tsx
+++ app/(routes)/(others)/mens/_components/product-basket.tsx
@@ -1,9 +1,12 @@
+"use client"
+
+import { useForTruthToggle } from '@/hooks'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const ProductBasket = () => {
     return (
-        <div className='flex justify-between gap-x-4 px-4'>
+        <div className='flex justify-between gap-x-20 px-20'>
             <LeftSide />
             <RightSide />
         </div>
@@ -12,11 +15,11 @@ export const ProductBasket = () => {
 
 const RightSide = () => {
     return (
-        <div>
+        <div className='bg-slate-400 w-1/2 flex flex-col gap-y-10 px-10 justify-center'>
             <div>
-                <p>Men's Mr T Organic T-shirt</p>
-                <div>
-                    <span>$25.00</span>
+                <p className='text-4xl font-bold'>Men's Mr T Organic T-shirt</p>
+                <div className='space-x-4 text-xl font-semibold'>
+                    <span className='line-through'>$25.00</span>
                     <span>$15.00</span>
                 </div>
             </div>
@@ -25,7 +28,7 @@ const RightSide = () => {
 
             <AddToBasket />
 
-            <Link href={"#"}>view product details</Link>
+            <Link href={"#"} className='underline text-xl'>view product details</Link>
 
         </div>
     )
@@ -34,29 +37,52 @@ const RightSide = () => {
 const AddToBasket = () => {
     return (
         <div className='flex gap-x-4'>
-            <div className='flex gap-x-2'>
-                <button>-</button>
-                <span>{1234}</span>
-                <button>+</button>
+            <div className='flex items-center justify-center gap-x-2 border text-xl'>
+                <button className='border-r-2 p-4 text-4xl'>-</button>
+                <span className='w-20 text-center'>{1234}</span>
+                <button className='border-l-2 p-4 text-4xl'>+</button>
             </div>
-            <button>Add to basket</button>
+            <button className='bg-slate-600 w-60'>Add to basket</button>
         </div>
     )
 }
 
 const Sizes = () => {
-    const allSizes = () => ["S", "M", "L", "XL", "XXL"].map(v => <div key={v} className='h-20 w-16'>{v}</div>)
+    const [text, setText] = useState("")
+
+    const updateText= (v:string) => setText(v)
+
+    // const allSizes = () => ["S", "M", "L", "XL", "XXL"].map(v => <div key={v} className='h-20 underline'>{v}</div>)
+
+    const allSizes = () => ["S", "M", "L", "XL", "XXL"].map(v => <SizeMarkup key={v} updateText={updateText} text={text}  v={v} />)
 
     return (
-        <div className='flex gap-x-4'>
-            {allSizes()}
+        <div className='flex flex-col gap-y-4'>
+            <p className='text-2xl font-bold'>Size</p>
+            <div className='flex gap-x-4'>
+                {allSizes()}
+            </div>
         </div>
+    )
+}
+
+const SizeMarkup = ({v, updateText, text}: {v: string, text: string, updateText: (d: string) => void}) => {
+    return (
+        <button 
+            key={v} 
+            // className={`h-20 ${v === text ? "underline" : ""} text-slate-800`} 
+            onClick={() => updateText(v)}
+            className='flex flex-col gap-y-1 justify-center items-center min-w-4'
+        >
+            <span className='text-center'>{v}</span>
+            <span className={`${v === text ? "visible" : "invisible"} h-0.5 w-full bg-slate-800`}></span>
+        </button>
     )
 }
 
 const LeftSide = () => {
     return (
-        <div className='flex gap-x-4'>
+        <div className='flex gap-x-4 w-1/2'>
             <div className='min-h-[27rem] bg-slate-400 min-w-96'></div>
             <div className='min-h-[27rem] bg-slate-400 min-w-96'></div>
         </div>
