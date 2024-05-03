13525878bbff9422ecbff3482bc8e01f6642ce3b
bappyasif <bappyasif@hotmail.com>
2024-05-03 21:21:00 +0600

    adjusted ui
    * adjusted add to basket components ui functionalities
    * adjusted product basket ui functionalities in general

--- app/(routes)/(others)/mens/_components/product-basket.tsx
+++ app/(routes)/(others)/mens/_components/product-basket.tsx
@@ -7,18 +7,24 @@ import React, { useEffect, useRef, useState } from 'react'
 import nxtImg from "@/public/next.svg"
 
 export const ProductBasket = () => {
+    const [rnd, setRnd] = useState(0.0)
+
+    useEffect(() => {
+        setRnd(Math.random())
+    }, [])
+
     return (
-        <div className='flex justify-between gap-x-20 px-20 relative'>
-            <LeftSide />
-            <RightSide />
+        <div className='flex justify-between gap-x-28 pl-20 relative'>
+            <LeftSide rnd={rnd} />
+            <RightSide rnd={rnd} />
         </div>
     )
 }
 
-const RightSide = () => {
+const RightSide = ({rnd}: {rnd: number}) => {
     return (
-        <div className='bg-slate-400 w-1/2 flex flex-col gap-y-10 px-10 justify-center'>
-            <div>
+        <div className='w-1/2 flex flex-col gap-y-14 px-10 justify-center'>
+            <div className='space-y-6'>
                 <p className='text-4xl font-bold'>Men's Mr T Organic T-shirt</p>
                 <div className='space-x-4 text-xl font-semibold'>
                     <span className='line-through'>$25.00</span>
@@ -28,7 +34,7 @@ const RightSide = () => {
 
             <Sizes />
 
-            <AddToBasket />
+            <AddToBasket rnd={rnd} />
 
             <Link href={"#"} className='underline text-xl'>view product details</Link>
 
@@ -36,15 +42,32 @@ const RightSide = () => {
     )
 }
 
-const AddToBasket = () => {
+const AddToBasket = ({rnd}: {rnd: number }) => {
+    const [count, setCount] = useState(0)
+
+    const increaseCount = () => {
+        setCount(prev => prev + 1)
+    }
+
+    const decreaseCount = () => {
+        setCount(prev => prev && prev - 1)
+    }
+
+    // const [rnd, setRnd] = useState(0.0)
+
+    // useEffect(() => {
+    //     setRnd(Math.random())
+    // }, [])
+
     return (
         <div className='flex gap-x-4'>
-            <div className='flex items-center justify-center gap-x-2 border text-xl'>
-                <button className='border-r-2 p-4 text-4xl'>-</button>
-                <span className='w-20 text-center'>{1234}</span>
-                <button className='border-l-2 p-4 text-4xl'>+</button>
+            <div className='flex items-center justify-center gap-x-2 border text-xl rounded-lg'>
+                <button className='p-4 text-4xl' onClick={decreaseCount}>-</button>
+                <span className='w-14 text-center'>{count}</span>
+                <button className='p-4 text-4xl' onClick={increaseCount}>+</button>
             </div>
-            <button className='bg-slate-600 w-60'>Add to basket</button>
+
+            <button disabled={rnd > .5} className={`${rnd > .5 ? "bg-slate-600" : "bg-slate-400"} w-60 rounded-lg`}>{rnd > .5 ? "Out of stock" : "Add to basket"}</button>
         </div>
     )
 }
@@ -61,7 +84,7 @@ const Sizes = () => {
 
     const updateMousePosition = (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         const test = ev.currentTarget.getBoundingClientRect()
-        console.log(test.x, test.y, test.top, test.left, "?!?!", ev.clientX - test.x)
+        // console.log(test.x, test.y, test.top, test.left, "?!?!", ev.clientX - test.x)
         setMousePosition({ x: ev.clientX - test.x > 0 ? ev.clientX - test.x : 0, y: ev.clientY });
     };
 
@@ -82,32 +105,37 @@ const Sizes = () => {
 
 
     return (
-        <div className='flex flex-col gap-y-0 w-fit'>
-            <p className='text-2xl font-bold'>Size</p>
-            <div
-                className='flex gap-x-4 w-fit h-fit'
-                onMouseMove={updateMousePosition}
-            >
-                {allSizes()}
+        <div className='w-fit'>
+            <div className='flex flex-col gap-y-4'>
+                <p className='text-2xl font-bold'>Size</p>
+                <div
+                    className='flex gap-x-4 w-fit h-fit relative'
+                    onMouseMove={updateMousePosition}
+                >
+                    {allSizes()}
 
+                    {/* <span
+                        className={`w-4 h-0.5 bg-purple-600 bottom-0 absolute -top-0.5`}
+                        style={{
+                            left: `${mousePosition.x! < 146 ? mousePosition.x : "146"}px`,
+                        }}
+                    ></span> */}
+
+                </div>
             </div>
+
             <div
                 // className='w-full h-1 relative bg-red-400'
                 className='w-full h-1 relative'
-            // onMouseMove={e => {
-            //     updateMousePosition(e)
-            // }}
             >
                 <span
-                    className={`w-4 h-0.5 bg-purple-600 bottom-0 absolute -top-0.5`}
+                    className={`${mousePosition.x! > 92 && mousePosition.x! < 96 ? "w-7" : mousePosition.x! > 129 && mousePosition.x! < 132 ? "w-10" : "w-4"} h-0.5 bg-purple-600 bottom-0 absolute -top-0.5`}
                     style={{
-                        // left: `${mousePosition.x! - 1000 > 0 ? mousePosition.x! - 1000 : 0}px`,
                         left: `${mousePosition.x! < 146 ? mousePosition.x : "146"}px`,
-                        // top: `${mousePosition.y! - 410}px`
-
                     }}
                 ></span>
             </div>
+
             {/* <span
                 // className={`w-4 h-1 bg-purple-600 bottom-0 left-[${mousePosition.x}px] top-[${mousePosition.y}px] absolute`}
                 className={`w-4 h-1 bg-purple-600 bottom-0 absolute`}
@@ -129,18 +157,24 @@ const SizeMarkup = ({ v, updateText, text }: { v: string, text: string, updateTe
             onClick={() => updateText(v)}
             className='flex flex-col gap-y-1 justify-center items-center min-w-4'
         >
-            <span className='text-center'>{v}</span>
+            <span className='text-center text-lg'>{v}</span>
             <span className={`${v === text ? "visible" : "invisible"} h-0.5 w-full bg-slate-800`}></span>
         </button>
     )
 }
 
-const LeftSide = () => {
+const LeftSide = ({rnd}: {rnd: number}) => {
+    // const [rnd, setRnd] = useState(0.0)
+    
+    // useEffect(() => {
+    //     setRnd(Math.random())
+    // }, [])
+
     // mensTeesProductImage
-    const pics = () => mensTeesProductImage.map(imgSrc => <PictureCard key={imgSrc} imgSrc={imgSrc} />)
+    const pics = () => mensTeesProductImage.map(imgSrc => <PictureCard key={imgSrc} imgSrc={imgSrc} rnd={rnd} />)
 
     return (
-        <div className='flex gap-x-4 w-1/2 overflow-x-scroll scrollbar'>
+        <div className='flex gap-x-4 w-2/3 overflow-x-scroll scrollbar'>
             {pics()}
 
             {/* <PictureCard imgSrc={mensTeesProductImage[0]} />
@@ -152,14 +186,14 @@ const LeftSide = () => {
     )
 }
 
-const PictureCard = ({ imgSrc }: { imgSrc: string }) => {
+const PictureCard = ({ imgSrc, rnd }: { imgSrc: string, rnd: number }) => {
     const { handleFalsy, handleTruthy, isTrue } = useForTruthToggle()
     return (
         <>
             <img
                 src={imgSrc}
                 alt={imgSrc}
-                className='min-h-[27rem] bg-slate-400 min-w-96' onClick={handleTruthy}
+                className={`min-h-[27rem] bg-slate-400 min-w-96 ${rnd > .5 ? "opacity-80" : "opacity-100"}`} onClick={handleTruthy}
 
                 style={{
                     cursor: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80"><circle cx="22" cy="22" r="22" opacity=".2" /><g><path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"/></g></svg>'), auto;`
